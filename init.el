d3ccf725d78cb9640d68532a454c98d1aac773b5
Bruno Fernandez-Ruiz <brunofr@yahoo-inc.com>
2014-06-16 12:48:32 +0100

    Increase window geometry width to 120 for long lines

--- init.el
+++ init.el
@@ -79,7 +79,7 @@
 ; default window width and height
 (defun custom-set-frame-size ()
   (add-to-list 'default-frame-alist '(height . 65))
-  (add-to-list 'default-frame-alist '(width . 99)))
+  (add-to-list 'default-frame-alist '(width . 120)))
 (custom-set-frame-size)
 (add-hook 'before-make-frame-hook 'custom-set-frame-size)
 
